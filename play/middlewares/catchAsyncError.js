3f18e35cd3feb99ad94b764682c2b15ec805639e
Bibek Bhattarai <bibekjodd@outlook.com>
2023-03-06 11:19:34 +0545

    removed play

--- play/middlewares/catchAsyncError.js
+++
@@ -1,5 +0,0 @@
-import ErrorHandler from "../utils/ErrorHandler.js";
-
-export const catchAsyncError = (passedFunction) => (req, res, next) => {
-    Promise.resolve(passedFunction(req, res, next)).catch(next);
-};
